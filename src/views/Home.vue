19b944de2b29a8ec4864412245dc6b3e1dbbec2f
takamori <takamori915@gmail.com>
2023-06-24 07:56:45 +0900

    datatable開発中

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,9 +6,54 @@
       <template v-slot:top>
         <v-toolbar flat>
           <v-toolbar-title>メモ一覧</v-toolbar-title>
+          <v-spacer />
+          <v-btn color="primary" dark class="mb-2" @click="addItem()"
+            >メモを追加</v-btn
+          >
         </v-toolbar>
       </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon small class="mr-2" @click="updateItem(item)">mdi-pencil</v-icon>
+        <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+      </template>
     </v-data-table>
+    <v-dialog v-model="dialogDelete" max-width="500px">
+      <v-card>
+        <v-card-title>この項目を削除してもよろしいですか？</v-card-title>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="blue darken-1" text @click="closeDialogDelete"
+            >キャンセル</v-btn
+          >
+          <v-btn color="blue darken-1" text @click="deleteItemAction"
+            >削除</v-btn
+          >
+          <v-spacer />
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="dialogEdit" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">{{ dialogTitle }}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                v-model="editItem.title"
+                label="タイトル"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          
+          <v-spacer />
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -26,6 +71,10 @@ export default {
     isLoading: false,
     items: [],
     errors: [],
+    dialogEdit: false,
+    dialogDelete: false,
+    editIndex: -1,
+    editItem: {},
     headers: [
       {
         text: "ステータス",
@@ -49,15 +98,26 @@ export default {
         text: "登録日",
         value: "createdAt",
         align: "center",
-        width: "300px",
+        width: "250px",
+      },
+      {
+        text: "更新日",
+        value: "updatedAt",
+        align: "center",
+        width: "250px",
       },
       {
         text: "",
-        value: "action",
+        value: "actions",
         align: "center",
       },
     ],
   }),
+  computed: {
+    dialogTitle() {
+      return this.editIndex === -1 ? "メモの追加" : "メモの更新";
+    },
+  },
   async mounted() {
     this.isLoading = true;
     const response = await axios
@@ -81,6 +141,30 @@ export default {
       });
     this.items = response.data.contents;
     return response;
+  },
+  methods: {
+    addItem() {
+      this.editIndex = -1;
+      this.editItem = {};
+      this.dialogEdit = true;
+    },
+    updateItem(item) {
+      this.editIndex = this.items.indexOf(item);
+      this.editItem = Object.assign({}, item);
+      this.dialogEdit = true;
+    },
+    deleteItem(item) {
+      this.editIndex = this.items.indexOf(item);
+      this.editItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+    closeDialogDelete() {
+      this.dialogDelete = false;
+    },
+    deleteItemAction() {
+      this.items.splice(this.editIndex, 1);
+      this.closeDialogDelete();
+    },
   },
 };
 </script>
